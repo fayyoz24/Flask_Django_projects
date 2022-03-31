383e7649e598a5ab0efd7f3ba23f2f8097c86dda
Fayyoz24 <fayyozusmon@gmail.com>
2022-03-31 10:07:28 +0200

    Create app.py

---
+++ D8_Flask_Rest/src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource
+from flask import request
+import uuid
+from routes.home.HomeRoute import HomeRoute
+
+db = SQLAlchemy()
+
+def create_app():
+
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    db.init_app(app) #initialize database
+    db.create_all(app=app)# create table
+    api = Api(app)
+    api.add_resource(HomeRoute, '/')
+   # api.add_resource(HomeRoutewithID, '/<string:id>')
+    return app
+
+
+# create_app()
+# if __name__== '__main__':
+#     app.run(debug=True)
